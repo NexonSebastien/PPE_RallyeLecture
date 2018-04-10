4a4b1c23ac636bbb8361066bc5260bfe4ae005dd
snexon <nexon.sebastien@gmail.com>
2018-04-10 12:12:59 +0200

    Add web ppe

--- RallyeLecture/PPE_RallyeLecture/Rally Lecture/rl/sql/rl/grant.sql
+++
@@ -1,7 +0,0 @@
- -- exemple de création d'un compte admin
- -- pour la base de données
- GRANT ALL PRIVILEGES 
- ON rallyeLecture.* 
- TO adminRallyeLecture@localhost
- IDENTIFIED BY 'siojjr' 
- WITH GRANT OPTION;
